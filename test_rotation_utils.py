caddf0da63cf246368bfdd85393dda448d46853e
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-10 12:57:57 -0400

    set up the imports and tests. i don't have pytest installed yet so everything's in comments.

--- test_rotation_utils.py
+++ test_rotation_utils.py
@@ -4,3 +4,34 @@ Ari Papke
 testing rotation_utils
 04/10/26
 """
+
+#import pytest
+from rotation_utils import adjust_rotation
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(100)
+    #assert adjusted_rotation == 100
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(460)
+    #assert adjusted_rotation == 100
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(820)
+    #assert adjusted_rotation == 100
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(-100)
+    #assert adjusted_rotation == 260
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(-460)
+    #assert adjusted_rotation == 260
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation(-820)
+    #assert adjusted_rotation == 260
+
+#def test_rotation_utils.py():
+    #adjusted_rotation = adjust_rotation("string")
+    #assert adjusted_rotation == TypeError
