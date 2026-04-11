32a86261fcbf6283f3514e7e11d7f564c4ba94ad
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-10 21:29:52 -0400

    installed pytest and got most of the tests functional

--- test_rotation_utils.py
+++ test_rotation_utils.py
@@ -5,33 +5,33 @@ testing rotation_utils
 04/10/26
 """
 
-#import pytest
+import pytest
 from rotation_utils import adjust_rotation
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(100)
-    #assert adjusted_rotation == 100
+def test_rotation_utils():
+    adjusted_rotation = adjust_rotation(100)
+    assert adjusted_rotation == 100
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(460)
-    #assert adjusted_rotation == 100
+def test_rotation_utils2():
+    adjusted_rotation = adjust_rotation(460)
+    assert adjusted_rotation == 100
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(820)
-    #assert adjusted_rotation == 100
+def test_rotation_utils3():
+    adjusted_rotation = adjust_rotation(820)
+    assert adjusted_rotation == 100
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(-100)
-    #assert adjusted_rotation == 260
+def test_rotation_utils4():
+    adjusted_rotation = adjust_rotation(-100)
+    assert adjusted_rotation == 260
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(-460)
-    #assert adjusted_rotation == 260
+def test_rotation_utils5():
+    adjusted_rotation = adjust_rotation(-460)
+    assert adjusted_rotation == 260
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation(-820)
-    #assert adjusted_rotation == 260
+def test_rotation_utils6():
+    adjusted_rotation = adjust_rotation(-820)
+    assert adjusted_rotation == 260
 
-#def test_rotation_utils.py():
-    #adjusted_rotation = adjust_rotation("string")
-    #assert adjusted_rotation == TypeError
+def test_rotation_utils7():
+    adjusted_rotation = adjust_rotation("string")
+    assert adjusted_rotation == TypeError
